71ffe509f1efc534fb743cf662bb8eddd7990eb2
Fresh Porridge <seth.corbridg3@gmail.com>
2021-10-12 10:00:39 -0600

    Created graphics code

--- software/netwon.py
+++ software/netwon.py
@@ -1,1 +1,23 @@
+import numpy as np
+def newton(x0, tol, maxiter):
+    expression = input('Enter expression:\n')
+    expp = input('Enter Derivative of expression:\n')
+    x = x0
+    f0=eval(expression)
+    fp0 = eval(expp)
+    error = tol * 10
+    iter=0
+    while error > tol and iter < maxiter:
+        x1 = x0 - f0/fp0
+        error = np.abs(x1-x0)
+        iter = iter + 1
+        x0 = x1
+        x=x0
+        f0 = eval(expression)
+        fp0 = eval(expp)
+    return x1
+
+print(newton(1.5,.1,1000))
+
+
 
--- software/secant.py
+++ software/secant.py
@@ -1,1 +1,22 @@
+import numpy as np
+def secant(x0, x1, tol, maxiter):
+    expression = input('Enter expression:\n')
+    x = x0
+    f0=eval(expression)
+    x = x1
+    f1 = eval(expression)
+    error = tol * 10
+    iter=0
+    while error > tol and iter < maxiter:
+        x2 = x1 - f1 * (x1-x0)/(f1-f0)
+        error = np.abs(x2-x1)
+        iter = iter + 1
+        x0 = x1
+        x=x0
+        f0 = eval(expression)
+        x1=x2
+        x = x1
+        f1 = eval(expression)
+    return x2
 
+print(secant(1,2,.01,1000))
