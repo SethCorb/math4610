842b277dd207ed49a92a25c580b33648795393b7
Fresh Porridge <seth.corbridg3@gmail.com>
2021-11-02 21:36:18 -0600

    yUP

--- software/hybrid.py
+++ software/hybrid.py
@@ -1,41 +1,47 @@
 import numpy as np
-def hybrid(a,b, tol, maxiter):
-    #expression = input('Enter expression:\n')
+
+
+def hybrid(a, b, tol, maxiter):
+    # expression = input('Enter expression:\n')
     expression = "np.exp(-x**2)*np.sin(4*x**2-1)+.051"
-    #expp = input('Enter Derivative of expression:\n')
+    # expp = input('Enter Derivative of expression:\n')
     expp = "8*x*np.exp(-x**2)*np.cos(4*x**2-1)-2*x*np.exp(-x**2)*np.sin(4*x**2-1)"
-    x0= (a+b)/2
+    x0 = (a + b) / 2
     x = x0
-    f0=eval(expression)
+    f0 = eval(expression)
     fp0 = eval(expp)
-    x1= x0-f0/fp0
-    e0=np.abs(b-a)
-    e1 = np.abs(x0-x1)
-    iter=0
-    err=5*tol
-    error=1000
+    x1 = x0 - f0 / fp0
+    e0 = np.abs(b - a)
+    e1 = np.abs(x0 - x1)
+    iter = 0
+    err = 5 * tol
+    error = 1000
     while err > tol and iter < maxiter:
-        x1 = x0 - f0/fp0
-        errnewt= np.abs(x1-x0)
+        x1 = x0 - f0 / fp0
+        errnewt = np.abs(x1 - x0)
         if errnewt > error:
             for k in range(4):
-                c=(a+b)/2
-                x=a
-                fa=eval(expression)
-                x=c
-                fc=eval(expression)
-                if fa*fc <0:
-                    b=c
-                    fb=fc
+                x = a
+                fa = eval(expression)
+                x = b
+                fb = eval(expression)
+                if fa * fb < 0:
+                    continue
+                c = (a + b) / 2
+                x = c
+                fc = eval(expression)
+                if fa * fc < 0:
+                    b = c
+                    fb = fc
                 else:
-                    a=c
-                    fa=fc
-                x0=(a+b)/2
-                    # finish bisection method.
+                    a = c
+                    fa = fc
+                x0 = (a + b) / 2
+                # finish bisection method.
 
-        x=x0
-        f0=eval(expression)
-        fp0=eval(expp)
+        x = x0
+        f0 = eval(expression)
+        fp0 = eval(expp)
         x1 = x0 - f0 / fp0
         error = np.abs(x1 - x0)
         iter = iter + 1
@@ -45,7 +51,5 @@ def hybrid(a,b, tol, maxiter):
         fp0 = eval(expp)
     return x1
 
-print(hybrid(-5,6,.001,1000))
 
-
-
+print(hybrid(-5, 6, .001, 1000))
