bc91dba9a6f6ed19e8a084513ac3ad9ef8ea4a6f
Fresh Porridge <seth.corbridg3@gmail.com>
2021-11-08 17:35:12 -0700

    yUP

--- software/HybridSecant.py
+++ software/HybridSecant.py
@@ -19,36 +19,59 @@ def hybrid(a, b, tol, maxiter):
     increment = .01
     lowbound=a
     x=lowbound
+    solist= []
     fl=eval(expression)
-    while err > tol and iter < maxiter:
-        x2 = x1 - f1 * (x1 - x0) / (f1 - f0)
-        errsec = np.abs(x2 - x1)
-        if errsec > error:
-            for k in range(4):
-                x = a
-                fa = eval(expression)
-                x = b
-                fb = eval(expression)
-                if fa * fb < 0:
-                    continue
-                c = (a + b) / 2
-                x = c
-                fc = eval(expression)
-                if fa * fc < 0:
-                    b = c
-                    fb = fc
-                else:
-                    a = c
-                    fa = fc
-                x0 = (a + b) / 2
-                # finish bisection method.
+    zerolist = []
+    while True:
+        upbound=lowbound+increment
+        x=upbound
+        fu=eval(expression)
 
-        x = x0
+        if fl*fu<0:
+            zerolist.append(lowbound)
+        lowbound+=increment
+        fl=eval(expression)
+        if lowbound > b:
+            break
+    fl = fu
+    for i in zerolist:
+        a = i
+        b = i+increment
+        x=a
+        x0=a
         f0 = eval(expression)
-        x = x1
+        x=b
+        x1=b
         f1 = eval(expression)
-        iter=0
-        while error > tol and iter < maxiter:
+        while err > tol and iter < maxiter:
+            x2 = x1 - f1 * (x1 - x0) / (f1 - f0)
+            errsec = np.abs(x2 - x1)
+            err = errsec
+            if errsec > error:
+                for k in range(4):
+                    x = a
+                    fa = eval(expression)
+                    x = b
+                    fb = eval(expression)
+                    if fa * fb < 0:
+                        continue
+                    c = (a + b) / 2
+                    x = c
+                    fc = eval(expression)
+                    if fa * fc < 0:
+                        b = c
+                        fb = fc
+                    else:
+                        a = c
+                        fa = fc
+                    x0 = (a + b) / 2
+                    # finish bisection method.
+
+            x = x0
+            f0 = eval(expression)
+            x = x1
+            f1 = eval(expression)
+            iter=0
             x2 = x1 - f1 * (x1 - x0) / (f1 - f0)
             error = np.abs(x2 - x1)
             iter = iter + 1
@@ -58,7 +81,11 @@ def hybrid(a, b, tol, maxiter):
             x1 = x2
             x = x1
             f1 = eval(expression)
-    return x1
+        solist.append(x1)
+    abslist = []
+    for i in solist:
+        abslist.append(np.abs(i))
+    return min(abslist)
 
 
 print(hybrid(-5, 6, .001, 1000))
