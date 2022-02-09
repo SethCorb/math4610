d908e3332c4b4ced68c0b0c6a187e6a9d3b9397b
Fresh Porridge <seth.corbridg3@gmail.com>
2022-02-09 14:48:17 -0700

    yUP

--- 5620/HW2.py
+++ 5620/HW2.py
@@ -1,6 +1,9 @@
+# Import needed packages
 import numpy as np
 import numpy.linalg as linalg
 import matplotlib.pyplot as plt
+
+# Define our matrix A
 A = np.zeros((11,11))
 A[0][0]=-.1
 A[0][1]=.1
@@ -11,25 +14,36 @@ for i in range(9):
 A[10][10]=.01
 A=A/.01
 
+# Define our vector F
 F = np.zeros((11,1))
 F[0]=1
 F[10]=1
 for i in range(1,10):
     F[i] = np.sin(i*.1)
 sol = linalg.solve(A,F)
+
+# Define x vector
 x = [(i-1)*.1 for i in range(1,12)]
+
+# plot the solution
 plt.plot(x,linalg.solve(A,F))
 plt.show()
+
+# Define real solution to determine error
 real = "-np.sin(x)+2*x-.1585"
+# Create rl as place holder for error
 rl = [0 for i in range(11)]
 for i in range(11):
     x= i*.1
     rl[i] = np.abs(eval(real) - sol[i][0])
 sum = 0
+
+# Find the 2-norm
 for i in rl:
     sum += i**2
 newsum = np.sqrt(sum*.1)
 x = [(i-1)*.1 for i in range(1,12)]
+# Plot error and print 2-norm
 print("2-norm:", newsum)
 plt.plot(x,rl)
 plt.show()
